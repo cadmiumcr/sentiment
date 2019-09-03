39e8f9db3ecde6f796ad75f7a655fd72c3e26766
Rémy Marronnier <rmarronnier@gmail.com>
2019-09-03 18:57:24 +0200

    "Tokenizer" string removed

--- src/cadmium/sentiment/sentiment.cr
+++ src/cadmium/sentiment/sentiment.cr
@@ -23,7 +23,7 @@ module Cadmium
     }
 
     # Manage the `Tokenizer` that the sentiment analyzer uses.
-    class_property tokenizer : Cadmium::Tokenizer = Cadmium::TreebankWordTokenizer.new
+    class_property tokenizer : Cadmium::Tokenizer = Cadmium::TreebankWord.new
 
     # Set the sentiment data. Format should look like:
     #
